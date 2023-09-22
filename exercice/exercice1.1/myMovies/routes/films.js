672390bfc882112a541a51a0eda1002b99ca92fd
Nicolas <nicolas.venturini@student.vinci.be>
2023-09-22 13:22:10 +0200

    1.1 API : read

---
+++ exercice/exercice1.1/myMovies/routes/films.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const FILMS = [
+    {
+        id : 1,
+        title : "Harry Potter",
+        duration : "2h",
+        budget : 362000,
+        link : "https://"
+    },
+    {
+        id : 2,
+        title : "Star Wars",
+        duration : "1h49",
+        budget : 529000,
+        link : "https://"
+    },
+    {
+        id : 3,
+        title : "Indiana Jones",
+        duration : "2h12",
+        budget : 713000,
+        link : "https://"
+    }
+]
+  
+  router.get('/', (req, res, next) => {
+    console.log("GET /films");
+    res.json(FILMS);
+  });
+
+module.exports = router;
