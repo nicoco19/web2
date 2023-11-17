903fc688068c19de226d22a175cd8b592c934641
Nicolas <nicolas.venturini@student.vinci.be>
2023-11-17 14:38:55 +0100

    finish : ex2.15

--- exercice/exercice2.15/js-router-boilerplate/src/Components/Pages/HomePage.js
+++ exercice/exercice2.15/js-router-boilerplate/src/Components/Pages/HomePage.js
@@ -1,6 +1,30 @@
 const HomePage = () => {
-  const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+
+ fetch('https://v2.jokeapi.dev/joke/Any?type=single')
+ .then((response) => {
+  if(!response.ok) throw Error(`il y a une erreur : ${response.status} : ${response.statusText}`);
+  return response.json();
+ })
+ .then((jokes) => {
+  addJoke(jokes);
+ })
+ .catch((err) => {
+  // eslint-disable-next-line no-console
+  console.log('il y a des erreurs !!!\n qui sont : ',err);
+ });
 };
+
+function addJoke(joke){
+const main = document.querySelector('main');
+main.innerHTML =  `
+<div class = "card text-center mb-3">
+<div class="card-body">
+<h5 class="card-title">Category : ${joke.category} </h5>
+  <p class = "card-text">  
+       Joke : ${joke.joke}</p>
+</div>
+</div>
+`;
+}
 
 export default HomePage;
